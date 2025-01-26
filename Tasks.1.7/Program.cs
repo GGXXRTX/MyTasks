4ae3c426e26c5ee977e0057da9013f01b222bf4e
Epsilon <avenom956@gmail.com>
2025-01-26 17:52:58 +0300

    Новые изменение, добавлены доп-массивы

---
+++ Tasks.1.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks._1._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя ");
+
+            string name = Console.ReadLine();
+                           //Lenght используется для нахождения индекса в имени, тоесть, Имя Ваня имеет индексы В - 0, А - 1, Н - 2, Я - 3
+            for (int i = name.Length - 1; i >= 0; i--)
+            {              //И каждый раз индекс будет идти назад, тоесть, этот цикл будет повторятся до тех пор, пока индекс не станет меньше 0
+                Console.Write(name[i] );
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasks.1.8_array+/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks._1._8_array_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+                //Создаем массив, дальше идут индексы массива
+                int[] number = { 1, 2, 5 };
+                //Внешне он выглядит так - int number (который равен 5, но дальше его значение поменяется) = 1 + 2
+                number[2] = number[0] + number[1];
+                //В итоге вывод будет 3, так как это сумма number[0] и number[1]
+                Console.WriteLine(number[2]);
+
+                Console.ReadLine();            
+        }
+    }
+}
