9610c29d0eaf8eee00f0c97b499adec4acd76a65
Epsilon <avenom956@gmail.com>
2025-01-26 21:22:03 +0300

    Проверил код, лично для себя

---
+++ GodotName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GodotName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1, b = 1, c = 1, d = 1;
+            a -= 1;    
+            b += 1;
+            c /= 2;
+            d *= 2; 
+            Console.WriteLine("Вывод переменной а = {0} \nВывод переменной b = {1} \nВывод переменной c = {2} \nВывод переменной d = {3} ", a, b, c, d);
+            Console.ReadLine();
+        }
+    }
+}
