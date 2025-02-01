7a0307f074c8856ff0f7d3afefec8b2d41d7528f
Epsilon <avenom956@gmail.com>
2025-02-02 01:28:39 +0300

    Ебанный С# и буквы

---
+++ TasksMyRepetition.1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksMyRepetition._1._2
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string inputString = Console.ReadLine().ToLower(); // Преобразуем в нижний регистр сразу
+
+            int vowelCount = 0;
+            int consonantCount = 0;
+
+            string vowels = "aeiou"; // Строка с гласными
+
+            foreach (char c in inputString)
+            {
+                if (char.IsLetter(c)) // Проверяем, является ли символ буквой
+                {
+                    if (vowels.Contains(c))
+                    {
+                        vowelCount++;
+                    }
+                    else
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Гласные: {vowelCount}");
+            Console.WriteLine($"Согласные: {consonantCount}");
+            Console.ReadLine();
+        }
+    }
+}
